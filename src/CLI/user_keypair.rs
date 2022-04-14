86f391fefcbbdc09d158a256578b30d0ebff86a4
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-04-14 23:31:41 +0100

    Accounts Pt.2

--- src/CLI/user_keypair.rs
+++ src/CLI/user_keypair.rs
@@ -2,18 +2,60 @@ use crate::{useful_funcs, Error};
 use ed25519_dalek::*;
 
 pub fn login() -> Result<Keypair, Error> {
-	let accounts_file_str = "accounts.json";
-	let accounts_file = std::path::PathBuf::from(accounts_file_str);
-	if accounts_file.exists() {
-		let file_data = read_and_decrypt(accounts_file_str, &get_password())?;
-		Keypair::from_bytes(&file_data)
-			.map_err(|err| Error::SignatureError(err))
-	} else {
-		Ok(get_keypair())
+	let accounts_dir = "accounts/";
+
+	// User input stuff
+	println!("Do you want to create a new account? (true/false)");
+	match text_io::try_read!() {
+		Ok(true) => create_account(accounts_dir),
+		Ok(false) => open_account(accounts_dir),
+		Err(_) => {
+			println!("Please only type true or false");
+			return login();
+		}
 	}
 }
 
-pub fn get_keypair() -> Keypair {
+pub fn create_account(accounts_dir: &str) -> Result<Keypair, Error> {
+	println!("Please create a password");
+	let first_password = get_password();
+	println!("Please repeat that password");
+	if first_password != get_password() {
+		println!("Passwords do not match.");
+		return create_account(accounts_dir)
+	}
+	let keypair = new_keypair();
+	encrypt_and_write(accounts_dir, &keypair.to_bytes(), &get_password())?;
+	Ok(keypair)
+}
+
+fn open_account(accounts_dir: &str) -> Result<Keypair, Error> {
+	let account_files: Vec<String> = std::fs::read_dir(accounts_dir)
+		.map_err(|err| Error::StdIo(err.kind()))?
+		.filter_map(get_and_print_str)
+		.collect();
+	let selection: String = text_io::try_read!().unwrap();
+	if account_files.contains(&selection) {
+		println!("Please type in the password for {}", selection);
+		let file_data = read_and_decrypt(&selection, &get_password())?;
+		Keypair::from_bytes(&file_data).map_err(|err| Error::SignatureError(err))
+	} else {
+		println!("Invalid selection, please pick a file");
+		login()
+	}
+}
+
+fn get_and_print_str(input: Result<std::fs::DirEntry, std::io::Error>) -> Option<String> {
+	let file = input.ok()?;
+	if !file.path().is_file() {
+		return None;
+	}
+	let file_name = file.file_name().to_str()?.to_owned();
+	println!("{}", file_name);
+	Some(file_name)
+}
+
+fn new_keypair() -> Keypair {
 	let secret_seed = get_random_from_usr();
 
 	let secret: SecretKey = SecretKey::from_bytes(&secret_seed[..SECRET_KEY_LENGTH]).unwrap();
@@ -42,7 +84,11 @@ fn get_password() -> [u8; 32] {
 }
 
 fn encrypt_and_write(file: &str, data_to_encrypt: &[u8], key: &[u8; 32]) -> Result<(), Error> {
-	use chacha20poly1305::{aead::{Aead, NewAead}, XChaCha20Poly1305};
+	// use chacha20poly1305::{aead::{Aead, NewAead}, XChaCha20Poly1305};
+	use chacha20poly1305::{
+		aead::{Aead, NewAead},
+		XChaCha20Poly1305,
+	};
 	use rand::{rngs::OsRng, RngCore};
 
 	let cipher = XChaCha20Poly1305::new(key.into());
@@ -61,7 +107,10 @@ fn encrypt_and_write(file: &str, data_to_encrypt: &[u8], key: &[u8; 32]) -> Resu
 }
 
 fn read_and_decrypt(file: &str, key: &[u8; 32]) -> Result<Vec<u8>, Error> {
-	use chacha20poly1305::{aead::{Aead, NewAead}, XChaCha20Poly1305};
+	use chacha20poly1305::{
+		aead::{Aead, NewAead},
+		XChaCha20Poly1305,
+	};
 
 	let cipher = XChaCha20Poly1305::new(key.into());
 
