34a6d3bcc4d11699a81c042dc07d2ecaa7b94939
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-10-29 09:23:32 +0100

    Format lib.rs to be alphabetical

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub mod custom_types;
 
 #[path = "backend/encrypt_decrypt.rs"]
 pub mod encrypt_decrypt;
+#[path = "backend/hex.rs"]
+pub mod hex;
 #[path = "backend/read.rs"]
 pub mod read;
 #[path = "backend/read_serde.rs"]
 pub mod read_serde;
 #[path = "backend/write_serde.rs"]
 pub mod write_serde;
-#[path = "backend/hex.rs"]
-pub mod hex;
